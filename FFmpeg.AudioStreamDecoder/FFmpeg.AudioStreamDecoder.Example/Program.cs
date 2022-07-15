40ea4594de726d89b2c3234a48bc0a58c13729fa
OleksandrNazaruk <o.nazaruk@engineer-service.tv>
2022-07-15 12:37:23 +0300

    first commit

--- FFmpeg.AudioStreamDecoder/FFmpeg.AudioStreamDecoder.Example/Program.cs
+++ FFmpeg.AudioStreamDecoder/FFmpeg.AudioStreamDecoder.Example/Program.cs
@@ -84,7 +84,7 @@ namespace FFmpeg.AudioStreamDecoder.Example
             }
 
             Console.ReadKey();
-            
+
         }
         private static unsafe void SetupLogging()
         {
